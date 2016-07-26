74c3e549dcdfecbaa7dfd6ff40b4c34ac91378a9
james <james.pollard@iinet.net.au>
2016-07-26 23:39:10 +1000

    progress update

--- database/database.sql
+++ database/database.sql
@@ -21,8 +21,12 @@ CREATE TABLE `location` (
   `lifeguard` tinyint(1) DEFAULT NULL,
   `lts` tinyint(1) DEFAULT NULL,
   `gym` tinyint(1) DEFAULT NULL,
+  `isc` tinyint(1) DEFAULT NULL,
   PRIMARY KEY (ID)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+ALTER TABLE `location`
+  ADD KEY `Location` (`Location`);
 
 INSERT INTO `location` (`ID`, `Location`, `lifeguard`, `lts`, `gym`) VALUES (1, 'None', 1, 1, 1);
 
@@ -40,7 +44,7 @@ CREATE TABLE `lifeguard` (
 
 ALTER TABLE `lifeguard`
   ADD KEY `staff1` (`staff1`),
-  ADD CONSTRAINT `staffFK` FOREIGN KEY (`staff1`) REFERENCES `users` (`username`);
+  ADD CONSTRAINT `lifeguard_ibfk_1` FOREIGN KEY (`staff1`) REFERENCES `users` (`username`) ON UPDATE CASCADE;
   
 /* create table LTS_Shift */
  
@@ -55,7 +59,12 @@ CREATE TABLE `LTS_Shift` (
   `end_date` date NOT NULL,
   PRIMARY KEY (ID)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-   
+
+ALTER TABLE `LTS_Shift`
+  ADD KEY `staff` (`staff`),
+  ADD KEY `staff_2` (`staff`),
+  ADD CONSTRAINT `staffFK` FOREIGN KEY (`staff`) REFERENCES `users` (`username`) ON UPDATE CASCADE;
+
 /* create table LTS_Cover */
 CREATE TABLE `LTS_Covers` (
   `ID` int(4) NOT NULL AUTO_INCREMENT,
@@ -68,17 +77,29 @@ CREATE TABLE `LTS_Covers` (
   PRIMARY KEY (ID)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+ALTER TABLE `LTS_Covers`
+  ADD KEY `staff` (`staff`),
+  ADD KEY `cover_for` (`cover_for`),
+  ADD CONSTRAINT `cover_foreign_key` FOREIGN KEY (`cover_for`) REFERENCES `users` (`username`) ON UPDATE CASCADE;
+
 /* create availability table */
 CREATE TABLE `availability` (
   `ID` int(5) NOT NULL,
   `username` varchar(65) NOT NULL,
   `monday` varchar(9) DEFAULT NULL,
+  `mondayNote` varchar(40) DEFAULT NULL,
   `tuesday` varchar(9) DEFAULT NULL,
+  `tuesdayNote` varchar(40) DEFAULT NULL,
   `wednesday` varchar(9) DEFAULT NULL,
+  `wednesdayNote` varchar(40) DEFAULT NULL,
   `thursday` varchar(9) DEFAULT NULL,
+  `thursdayNote` varchar(40) DEFAULT NULL,
   `friday` varchar(9) DEFAULT NULL,
+  `fridayNote` varchar(40) DEFAULT NULL,
   `saturday` varchar(9) DEFAULT NULL,
+  `saturdayNote` varchar(40) DEFAULT NULL,
   `sunday` varchar(9) DEFAULT NULL,
+  `sundayNote` varchar(40) DEFAULT NULL,
   `department` varchar(9) DEFAULT NULL,
   `location` varchar(65) NOT NULL,
   `weekStarting` date NOT NULL,
@@ -86,10 +107,11 @@ CREATE TABLE `availability` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 ALTER TABLE `availability`
-ADD KEY `username` (`username`);
+ADD KEY `username` (`username`),
+ADD CONSTRAINT `usernameFK` FOREIGN KEY (`username`) REFERENCES `users` (`username`) ON UPDATE CASCADE;
 
 /* create ISC table */
-CREATE TABLE `ISC` (
+CREATE TABLE `isc` (
   `ID` int(4) NOT NULL,
   `shift_date` date NOT NULL,
   `start_time` time NOT NULL,
@@ -107,7 +129,11 @@ ALTER TABLE `ISC`
   ADD KEY `staff1` (`staff1`),
   ADD KEY `staff2` (`staff2`),
   ADD KEY `staff3` (`staff3`),
-  ADD KEY `staff4` (`staff4`);
+  ADD KEY `staff4` (`staff4`),
+  ADD CONSTRAINT `staff4FK` FOREIGN KEY (`staff4`) REFERENCES `users` (`username`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `staff1FK` FOREIGN KEY (`staff1`) REFERENCES `users` (`username`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `staff2FK` FOREIGN KEY (`staff2`) REFERENCES `users` (`username`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `staff3FK` FOREIGN KEY (`staff3`) REFERENCES `users` (`username`) ON UPDATE CASCADE;
 
 /* create new user
  change address and password to desired values */
